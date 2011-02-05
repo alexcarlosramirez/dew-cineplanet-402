833d1846a0dd660e4b98fc76d1ae005d1e23c0d7
alexancarlProject <alexancarlProject@f125073d-8ef2-eb3b-002d-e97413351d53>
2011-02-05 18:26:30 +0000

    Se agrega javadoc

--- dw2011cp007/src/main/java/pe/edu/upc/dw2011cp007/repository/UsuarioRepository.java
+++ dw2011cp007/src/main/java/pe/edu/upc/dw2011cp007/repository/UsuarioRepository.java
@@ -2,11 +2,20 @@ package pe.edu.upc.dw2011cp007.repository;
 
 import pe.edu.upc.dw2011cp007.model.UsuarioModel;
 
+/**
+ * Clase encargada de la administración del UsuarioModel
+ * @author u201021418
+ *
+ */
 public class UsuarioRepository {
 
+	/**
+	 * Graba usuario
+	 * @param usuario
+	 */
 	public void grabarUsuario(UsuarioModel usuario) {
 		System.out.println("Se espera registrar el usuario " + usuario.getNombreUsuario());
-		usuario.setIdUsuario(1);
+		usuario.setIdUsuario(50);//si grabamos usuario, el id debe ser mayor a cero
 		System.out.println("Se registró el usuario " + usuario.getIdUsuario());
 	}
 }
