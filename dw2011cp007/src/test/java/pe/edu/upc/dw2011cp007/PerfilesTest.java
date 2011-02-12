5b121ed07370204960781e6ef661e71cfd675438
miguelmac20 <miguelmac20@f125073d-8ef2-eb3b-002d-e97413351d53>
2011-02-12 11:33:21 +0000

    Prueba a la clase categoria q no permite dplucados

--- dw2011cp007/src/test/java/pe/edu/upc/dw2011cp007/PerfilesTest.java
+++ dw2011cp007/src/test/java/pe/edu/upc/dw2011cp007/PerfilesTest.java
@@ -12,28 +12,65 @@ import pe.edu.upc.dw2011cp007.repository.PerfilRepository;
 
 public class PerfilesTest {
 	
-	@Before
-	public void funciones(){
+	PerfilRepository repository= new PerfilRepository();
 	
-	}
 	
 	@Test
 	public void actualizarPerfil(){
-		PerfilRepository repository= new PerfilRepository();
+		
 		
 	}
 	
 	@Test
 	public void grabarperfil(){
-		PerfilRepository repository= new PerfilRepository();
+		//PerfilRepository repository= new PerfilRepository();
 		int valnombre=0;
 		int retorno=0;
 		HashMap<Integer, String> listadofuncion= new HashMap<Integer, String>();
+		
+		//SIMULACION CREACION DEL PRIMER PERFIL
 		PerfilModel perfilmodel= new PerfilModel();
 		valnombre=perfilmodel.setNombrePerfil("Administrador");
 		perfilmodel.setDescripcionPerfil("Perfil de prueba");
 		perfilmodel.setVigenciaPerfil(30);
-		perfilmodel.setEstadoPerfil(1);
+		perfilmodel.setListadofuncion(new HashMap<Integer, String>());
+		/*Se espera que la longitud de nom. perfil 
+		 * sea de 5 a 30 caracteres 
+		 */
+		assertTrue("Error en longitud Nombre de Perfil", valnombre==0);
+		retorno=repository.grabarperfil(1,perfilmodel);
+		assertTrue("No se grabo el perfil error: "+ retorno,retorno==0);
+		
+		//=============================================================
+		//SIMULACION CREACION DEL SEGUNDO PERFIL
+		PerfilModel perfilmodel2= new PerfilModel();
+		valnombre=perfilmodel2.setNombrePerfil("Operador");
+		perfilmodel2.setDescripcionPerfil("Perfil de prueba");
+		perfilmodel2.setVigenciaPerfil(30);
+		perfilmodel2.setEstadoPerfil(1);
+		perfilmodel2.setListadofuncion(new HashMap<Integer, String>());
+		/*Se espera que la longitud de nom. perfil 
+		 * sea de 5 a 30 caracteres 
+		 */
+		assertTrue("Error en longitud Nombre de Perfil", valnombre==0);
+		retorno=repository.grabarperfil(2,perfilmodel2);
+		assertTrue("No se grabo el perfil error: "+ retorno,retorno==0);
+	}
+	
+	@Test
+	public void asignarfunciones(){
+		int val=0;
+		grabarperfil();
+		val=repository.asignarfuncion("Administrador","agregar_usuarios");
+		assertTrue("Funcion ya asignada al Perfil",val==0);
+		val=repository.asignarfuncion("Administrador","agregar_usuarios2");
+		assertTrue("Funcion ya asignada al Perfil",val==0);
+		val=repository.asignarfuncion("Operario","agregar_usuarios3");
+		assertTrue("Funcion ya asignada al Perfil",val==0);
+		val=repository.asignarfuncion("Operario","agregar_usuarios4");
+		assertTrue("Funcion ya asignada al Perfil",val==0);
+		System.out.println("Funcion Asignada");
+		/*
 		listadofuncion.put(1, "agregar_usuarios");
 		listadofuncion.put(2, "eliminar_usuarios");
 		listadofuncion.put(4, "actualizar_usuarios");
@@ -44,36 +81,6 @@ public class PerfilesTest {
 		listadofuncion.put(9, "reinicilizar_password");
 		listadofuncion.put(10, "otro");
 		perfilmodel.setListadofuncion(listadofuncion);
-	
-		/*Se espera que la longitud de nom. perfil 
-		 * sea de 5 a 30 caracteres 
-		 */
-		assertTrue("Error en Nombre de Perfil", valnombre==0);
-		retorno=repository.grabarperfil(1,perfilmodel);
-		assertTrue("No se grabo el perfil error: "+ retorno,retorno==0);
-		
-		//Grabando Segundo Perfil
-		PerfilModel perfilmodel2= new PerfilModel();
-		valnombre=perfilmodel2.setNombrePerfil("Operador");
-		perfilmodel2.setDescripcionPerfil("Perfil de prueba");
-		perfilmodel2.setVigenciaPerfil(30);
-		perfilmodel2.setEstadoPerfil(1);
-		listadofuncion.put(1, "agregar_usuarios");
-		listadofuncion.put(2, "eliminar_usuarios");
-		listadofuncion.put(4, "actualizar_usuarios");
-		listadofuncion.put(5, "agregar_peliculas");
-		listadofuncion.put(6, "eliminar_peliculas");
-		listadofuncion.put(7, "actualizar_peliculas");
-		listadofuncion.put(8, "actualizar_cartelera");
-		listadofuncion.put(9, "reinicilizar_password");
-		listadofuncion.put(10, "otro");
-		perfilmodel2.setListadofuncion(listadofuncion);
-	
-		/*Se espera que la longitud de nom. perfil 
-		 * sea de 5 a 30 caracteres 
-		 */
-		assertTrue("Error en Nombre de Perfil", valnombre==0);
-		retorno=repository.grabarperfil(2,perfilmodel2);
-		assertTrue("No se grabo el perfil error: "+ retorno,retorno==0);
+		*/
 	}
 }
