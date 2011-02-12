7ee32f46254a76678b4edf598007158f0543fd0d
miguelmac20 <miguelmac20@f125073d-8ef2-eb3b-002d-e97413351d53>
2011-02-12 02:50:09 +0000

    Comprueba que no exista Perfiles duplicados

--- dw2011cp007/src/test/java/pe/edu/upc/dw2011cp007/PerfilesTest.java
+++ dw2011cp007/src/test/java/pe/edu/upc/dw2011cp007/PerfilesTest.java
@@ -18,14 +18,16 @@ public class PerfilesTest {
 	}
 	
 	@Test
-	public void asignaPerfil(){
-		
+	public void actualizarPerfil(){
+		PerfilRepository repository= new PerfilRepository();
 		
 	}
 	
 	@Test
 	public void grabarperfil(){
+		PerfilRepository repository= new PerfilRepository();
 		int valnombre=0;
+		int retorno=0;
 		HashMap<Integer, String> listadofuncion= new HashMap<Integer, String>();
 		PerfilModel perfilmodel= new PerfilModel();
 		valnombre=perfilmodel.setNombrePerfil("Administrador");
@@ -47,10 +49,31 @@ public class PerfilesTest {
 		 * sea de 5 a 30 caracteres 
 		 */
 		assertTrue("Error en Nombre de Perfil", valnombre==0);
+		retorno=repository.grabarperfil(1,perfilmodel);
+		assertTrue("No se grabo el perfil error: "+ retorno,retorno==0);
 		
-		PerfilRepository repository= new PerfilRepository();
-		repository.grabarperfil(perfilmodel);
-		
-		
+		//Grabando Segundo Perfil
+		PerfilModel perfilmodel2= new PerfilModel();
+		valnombre=perfilmodel2.setNombrePerfil("Operador");
+		perfilmodel2.setDescripcionPerfil("Perfil de prueba");
+		perfilmodel2.setVigenciaPerfil(30);
+		perfilmodel2.setEstadoPerfil(1);
+		listadofuncion.put(1, "agregar_usuarios");
+		listadofuncion.put(2, "eliminar_usuarios");
+		listadofuncion.put(4, "actualizar_usuarios");
+		listadofuncion.put(5, "agregar_peliculas");
+		listadofuncion.put(6, "eliminar_peliculas");
+		listadofuncion.put(7, "actualizar_peliculas");
+		listadofuncion.put(8, "actualizar_cartelera");
+		listadofuncion.put(9, "reinicilizar_password");
+		listadofuncion.put(10, "otro");
+		perfilmodel2.setListadofuncion(listadofuncion);
+	
+		/*Se espera que la longitud de nom. perfil 
+		 * sea de 5 a 30 caracteres 
+		 */
+		assertTrue("Error en Nombre de Perfil", valnombre==0);
+		retorno=repository.grabarperfil(2,perfilmodel2);
+		assertTrue("No se grabo el perfil error: "+ retorno,retorno==0);
 	}
 }
