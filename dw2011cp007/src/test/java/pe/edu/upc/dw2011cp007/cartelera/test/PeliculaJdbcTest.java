e77d64bf00e52607e72cbbde8631aa216571ac16
alexancarlProject <alexancarlProject@f125073d-8ef2-eb3b-002d-e97413351d53>
2011-03-05 18:39:09 +0000

    Test para PeliculaRepository

--- dw2011cp007/src/test/java/pe/edu/upc/dw2011cp007/cartelera/test/PeliculaJdbcTest.java
+++ dw2011cp007/src/test/java/pe/edu/upc/dw2011cp007/cartelera/test/PeliculaJdbcTest.java
@@ -16,32 +16,13 @@ import pe.edu.upc.dw2011cp007.cartelera.model.PeliculaModel;
 import pe.edu.upc.dw2011cp007.cartelera.service.CarteleraService;
 import pe.edu.upc.dw2011cp007.mantenimiento.model.ArtistaModel;
 
+
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(
-		locations={
-				"classpath:pe/edu/upc/dw2011cp007/config/test-infrastructure-config.xml",
-				"classpath:pe/edu/upc/dw2011cp007/config/application-config.xml"})
+@ContextConfiguration(locations={"classpath:pe/edu/upc/dw2011cp007/config/application-config.xml"})
 public class PeliculaJdbcTest {
 
 	@Autowired
-	CarteleraService carteleraService;
-
-	/**
-	 * Muestra la lista de peliculas a proyectarse el día de hoy.
-	 */
-	@Test
-	public void buscarListaPeliculaEnCartelera() {
-		ArrayList<PeliculaModel> listaPelicula = carteleraService.buscarListaPeliculaEnCartelera();
-		assertTrue(listaPelicula.size()>0);
-
-		System.out.println("==========================================================");
-		System.out.println("@Test buscarListaPeliculaEnCartelera");
-		System.out.println("Se encontraron " + listaPelicula.size() + " peliculas.");
-		for (PeliculaModel peliculaModel : listaPelicula) {
-			System.out.println(peliculaModel);
-		}
-		System.out.println("==========================================================");
-	}
+	private CarteleraService carteleraService;
 
 	/**
 	 * Cuando selecciona una pelicula se envía el id de este y se busca la información.
@@ -49,6 +30,7 @@ public class PeliculaJdbcTest {
 	@Test
 	public void buscarPeliculaSeleccionada() {
 		int idPeliculaSel = 5;
+
 		PeliculaModel peliculaSel = carteleraService.buscarPeliculaPorId(idPeliculaSel);
 		assertNotNull(peliculaSel);
 
@@ -66,6 +48,41 @@ public class PeliculaJdbcTest {
 		System.out.println("Actuan:");
 		for (ArtistaModel artistaModel : listaArtistaModel) {
 			System.out.println(artistaModel.getNombreartista());
+		}
+		System.out.println("==========================================================");
+	}
+
+	/**
+	 * Muestra la lista de peliculas en cartelera.
+	 */
+	@Test
+	public void buscarListaPeliculaEnCartelera() {
+		ArrayList<PeliculaModel> listaPelicula = carteleraService.buscarListaPeliculaEnCartelera();
+		assertTrue(listaPelicula.size()>0);
+
+		System.out.println("==========================================================");
+		System.out.println("@Test buscarListaPeliculaEnCartelera");
+		System.out.println("Se encontraron " + listaPelicula.size() + " peliculas.");
+		for (PeliculaModel peliculaModel : listaPelicula) {
+			System.out.println(peliculaModel);
+		}
+		System.out.println("==========================================================");
+	}
+
+	/**
+	 * Permite al usuario consultar la lista de cines y la programación de peliculas de cada uno.
+	 * Se considera pelicula en estreno aquella que tiene se proyectan hoy por primera vez
+	 */
+	@Test
+	public void buscarListaPeliculaEnEstreno() {
+		ArrayList<PeliculaModel> listaPelicula = carteleraService.buscarListaPeliculaEnEstreno();
+		assertTrue(listaPelicula.size()>0);
+
+		System.out.println("==========================================================");
+		System.out.println("@Test buscarListaPeliculaEnEstreno");
+		System.out.println("Se encontraron " + listaPelicula.size() + " peliculas.");
+		for (PeliculaModel peliculaModel : listaPelicula) {
+			System.out.println(peliculaModel);
 		}
 		System.out.println("==========================================================");
 	}
