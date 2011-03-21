3e00170b606ea13e859d748c9dafebfe7582cd39
yonni.lv <yonni.lv@f125073d-8ef2-eb3b-002d-e97413351d53>
2011-03-21 01:44:47 +0000

    COMMIT POR SIACA

--- dw2011cp007/src/test/java/pe/edu/upc/dw2011cp007/UserInternoTest.java
+++
@@ -1,7 +0,0 @@
-package pe.edu.upc.dw2011cp007;
-
-import pe.edu.upc.dw2011cp007.seguridad.repository.UsuarioRepository;
-
-public class UserInternoTest {
-	UsuarioRepository repository = new UsuarioRepository();
-}
